7ce427fe2230c738d6890aa44b4b898fe07a7081
DUTTASOF <148446432+DUTTASOF@users.noreply.github.com>
2023-10-19 20:23:16 +0530

    Update streamlit.app.py
    Added few components to the app

--- streamlit.app.py
+++ streamlit.app.py
@@ -1,3 +1,9 @@
 import streamlit
 streamlit.title ('My parents New Healthy Dinner')
+streamlit.header ('Breakfast Menu')
+streamlit.text ('Omega 3 & Blueberry oatmeal')
+streamlit.text ('Kale, spinach & Rocket Smoothie ')
+streamlit.text ('Hard bolied free-Range Egg')
+
+                
 
