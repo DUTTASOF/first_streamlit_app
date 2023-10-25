91f345a05938e498adf5fb52a3d8bb47e4a6f2b1
DUTTASOF <148446432+DUTTASOF@users.noreply.github.com>
2023-10-25 12:29:42 +0530

    Update streamlit.app.py
    Update Kiwi

--- streamlit.app.py
+++ streamlit.app.py
@@ -23,3 +23,8 @@ streamlit.text(fruityvice_response.json())
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
