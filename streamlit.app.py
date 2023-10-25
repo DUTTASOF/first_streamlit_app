fac326ca212aeba963246b0dfff0e67b4573181e
DUTTASOF <148446432+DUTTASOF@users.noreply.github.com>
2023-10-25 19:07:14 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -61,6 +61,14 @@ my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-streamlit.write ('Thanks for adding', add_my_fruit)
-my_cur.execute("insert into FRUIT_LOAD_LIST values ('from streamlit')")
+from urllib.error import URLEroor
+streamlit.header('Fruityvice Fruit Advice!')
+try:
+  fruit_choice =  streamlit.text.input('what fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("please select a fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
