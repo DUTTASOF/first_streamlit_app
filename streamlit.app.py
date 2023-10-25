3440fc65f284108a778c869ee64b06420c560af8
DUTTASOF <148446432+DUTTASOF@users.noreply.github.com>
2023-10-25 19:07:45 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -56,10 +56,6 @@ my_data_row = my_cur.fetchone()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.text("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
 
 from urllib.error import URLEroor
 streamlit.header('Fruityvice Fruit Advice!')
